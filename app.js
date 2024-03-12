f3ef142a79d50995a161bd21bb51c58b9d027ec1
Hasan <hderhamy@gmail.com>
2024-03-12 01:01:36 +0100

    tinyhttp server with a simple index.html working.

---
+++ app.js
@@ -0,0 +1,30 @@
+import { App } from '@tinyhttp/app'
+import { logger } from '@tinyhttp/logger'
+import path from 'node:path'
+
+
+const app = new App()
+
+app
+  .use(logger())
+  .get(
+    '/',
+    (_, res) =>
+      void res.format({
+        //html: () => res.send('<h1>Hello World</h1>'),
+        html: () => res.sendFile(path.resolve('public/index.html'), {}, (err) => {
+          if (err) console.error(err)
+        }),
+        text: () => res.send('Hello World')
+      })
+  )
+  .get('/page/:page/', (req, res) => {
+    res.status(200).send(`
+    <h1>Some cool page</h1>
+    <h2>URL</h2>
+    ${req.url}
+    <h2>Params</h2>
+    ${JSON.stringify(req.params, null, 2)}
+  `)
+  })
+  .listen(3000, () => console.log(`Listening on http://localhost:3000`))
